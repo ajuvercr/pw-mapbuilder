95e5e898b53d5564798e7a67c1d5be933c58d817
ajuvercr <arthur.vercruysse@ugent.be>
2022-08-23 21:39:51 +0200

    bump

--- src/bin/mapbuilder.rs
+++ src/bin/mapbuilder.rs
@@ -1,15 +1,60 @@
-
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{
+    ecs::system::Resource, prelude::*, sprite::MaterialMesh2dBundle, window::RequestRedraw,
+    winit::WinitSettings,
+};
 use bevy_egui::EguiPlugin;
 use mapbuilder::{
-    self, input, map_config::MapConfig, CurrentPlayer, HoverPlanet,
-    Location, HoveringUI,
+    self, input, map_config::MapConfig, CurrentPlayer, HoverPlanet, HoveringUI, Location,
 };
 
+#[derive(Debug)]
+enum ExampleMode {
+    Game,
+    Application,
+    ApplicationWithRedraw,
+}
+
+/// Update winit based on the current `ExampleMode`
+fn update_winit(
+    mode: Res<ExampleMode>,
+    mut event: EventWriter<RequestRedraw>,
+    mut winit_config: ResMut<WinitSettings>,
+) {
+    use ExampleMode::*;
+    *winit_config = match *mode {
+        Game => {
+            // In the default `WinitConfig::game()` mode:
+            //   * When focused: the event loop runs as fast as possible
+            //   * When not focused: the event loop runs as fast as possible
+            WinitSettings::game()
+        }
+        Application => {
+            // While in `WinitConfig::desktop_app()` mode:
+            //   * When focused: the app will update any time a winit event (e.g. the window is
+            //     moved/resized, the mouse moves, a button is pressed, etc.), a [`RequestRedraw`]
+            //     event is received, or after 5 seconds if the app has not updated.
+            //   * When not focused: the app will update when the window is directly interacted with
+            //     (e.g. the mouse hovers over a visible part of the out of focus window), a
+            //     [`RequestRedraw`] event is received, or one minute has passed without the app
+            //     updating.
+            WinitSettings::desktop_app()
+        }
+        ApplicationWithRedraw => {
+            // Sending a `RequestRedraw` event is useful when you want the app to update the next
+            // frame regardless of any user input. For example, your application might use
+            // `WinitConfig::desktop_app()` to reduce power use, but UI animations need to play even
+            // when there are no inputs, so you send redraw requests while the animation is playing.
+            event.send(RequestRedraw);
+            WinitSettings::desktop_app()
+        }
+    };
+}
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins)
+    app.insert_resource(WinitSettings::desktop_app())
+        .insert_resource(ExampleMode::ApplicationWithRedraw)
+        .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .add_plugin(mapbuilder::background::BackgroundPlugin)
         .add_startup_system(setup)
@@ -20,7 +65,8 @@ fn main() {
         .add_system(input::spawn_planet)
         .add_system(input::change_bg_color)
         .add_system(ui::ui_system)
-        .add_system(fps);
+        .add_system(fps)
+        .add_system(update_winit);
 
     app.run();
 }
@@ -122,13 +168,18 @@ fn setup(
     color.set_a(0.4);
     commands
         .spawn_bundle(MaterialMesh2dBundle {
-            mesh: meshes
-                .add(config.mesh())
-                .into(),
+            mesh: meshes.add(config.mesh()).into(),
             material: materials.add(ColorMaterial::from(color)),
             ..default()
         })
-        .insert_bundle((HoverPlanet, Location { x: 0, y: 0, player: None }));
+        .insert_bundle((
+            HoverPlanet,
+            Location {
+                x: 0,
+                y: 0,
+                player: None,
+            },
+        ));
 
     let transform = Transform::from_xyz(0.0, 0.0, 1000.0).with_scale(Vec3::new(
         1. / config.zoom,
@@ -149,4 +200,3 @@ fn transform_hover_planet(
         *transform = config.location_to_transform(loc, 0.1);
     }
 }
-
